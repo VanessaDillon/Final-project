2a8d316e64e7d3699748e2bb714fe70cf5c2fa68
Vanessa Dillon <vanessadillonmalisha@gmail.com>
2019-04-12 14:39:45 -0500

    first commit

---
+++ slick.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready( () =>{
+    $('.slick-slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+  });
+ 
+   
